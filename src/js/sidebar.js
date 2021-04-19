102fc5d84e67b51eace1e7ec33bbb224a8297dfa
Borisenko Anton <aborisenko@xbsoftware.by>
2021-04-19 22:06:06 +0300

    [fix] icons color

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -2,7 +2,7 @@ import {dataSidebar} from "../data/dataSidebar";
 
 export const sidebar = {
 	view: "sidebar",
-	css: "webix_dark",
+	css: "webix_dark leftSidebar",
 	select: true,
 	width: 204,
 	data: dataSidebar,
