1cbba22b3937076fc7e4bed73b054cd39ffb684d
Borisenko Anton <aborisenko@xbsoftware.by>
2021-04-19 22:04:09 +0300

    Revert "[fix] icons color"
    This reverts commit ee433bb9b9d4dacb154a44e8cb2d2de8ce6c56d2.

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -2,7 +2,7 @@ import {dataSidebar} from "../data/dataSidebar";
 
 export const sidebar = {
 	view: "sidebar",
-	css: "webix_dark leftSidebar",
+	css: "webix_dark",
 	select: true,
 	width: 204,
 	data: dataSidebar,
